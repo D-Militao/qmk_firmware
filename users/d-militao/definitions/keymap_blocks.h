862e132ce02fff2a746154bd6d5713804e720bef
D-Militao <daniel.militao95@gmail.com>
2023-04-08 20:59:38 +0200

    Switched shift and ctrl in game layer

--- users/d-militao/definitions/keymap_blocks.h
+++ users/d-militao/definitions/keymap_blocks.h
@@ -18,8 +18,8 @@
 
 #define _____GAME_L5_____ KC_GRV,  KC_1, KC_2, KC_3, KC_4, KC_5
 #define _____GAME_L4_____ KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T
-#define _____GAME_L3_____ KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G
-#define _____GAME_L2_____ KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B
+#define _____GAME_L3_____ KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G
+#define _____GAME_L2_____ KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B
 #define _____GAME_L1_____ KC_ESC, KC_SPC, LT(NAV, KC_TAB)
 
 #define _____GAME_R5_____ KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL
